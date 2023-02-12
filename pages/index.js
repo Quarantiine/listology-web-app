326f803d17a104d7d9885aeb42723cbab56f6c0b
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2023-02-12 02:18:33 -0600

    sm change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Listology | Home</title>
+				<title>Listology</title>
 				<meta name="description" content="Welcome to Listology" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/icons/simple-icons/icon logo.svg" />
