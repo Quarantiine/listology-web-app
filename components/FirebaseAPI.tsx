98922a5e74dc2f275aae92e7e0340bd75f09352b
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2023-02-21 12:40:06 -0600

    setting up finished

---
+++ components/FirebaseAPI.tsx
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	onSnapshot,
+	addDoc,
+	updateDoc,
+	deleteDoc,
+	doc,
+	orderBy,
+	where,
+	query,
+} from "firebase/firestore";
+
+const FirebaseAPI = () => {
+	const firebaseConfig = {
+		apiKey: "AIzaSyAs56GPO60nYErJxefy9853HWz17bGPSho",
+		authDomain: "listology-787f6.firebaseapp.com",
+		projectId: "listology-787f6",
+		storageBucket: "listology-787f6.appspot.com",
+		messagingSenderId: "784456370033",
+		appId: "1:784456370033:web:daf0c39df03edd2b6b98d0",
+		measurementId: "G-X8VS02Q4Q6",
+	};
+
+	const app = initializeApp(firebaseConfig);
+	const db = getFirestore(app);
+	const colRef = collection(db, "");
+
+	class TodoListSystem {
+		constructor() {}
+	}
+
+	class FolderSystem {
+		constructor() {}
+	}
+
+	return {};
+};
+
+export default FirebaseAPI;
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Listology</title>
-				<meta name="description" content="Welcome to Listology" />
+				<meta name="description" content="Welcome to Listology, The Todo List Web Application" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/icons/simple-icons/icon logo.svg" />
 			</Head>
